092447deb41441be15718915a8bada2a5b7573fd
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-12-05 05:55:15 +0800

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = express.Router();
+const bodyParser = require("body-Parser");
 const passport = require("passport");
 const crypto = require("crypto");
 const async = require("async");
